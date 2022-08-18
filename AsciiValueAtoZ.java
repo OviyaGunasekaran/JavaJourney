c87192b6b3cef264b78de14cad2e3e29116e27cb
OviyaGunasekaran <ovybusybee@gmail.com>
2022-08-18 18:14:02 +0530

    ASCII value - User input, all chars Codes added

---
+++ AsciiValueAtoZ.java
@@ -0,0 +1,19 @@
+package Oviya;
+
+public class AsciiValueAtoZ {
+	
+	 
+	public static void main(String[] args)   
+	{  
+	for(int i = 65; i <= 90; i++)   // to see all values  try the range 0 to 255
+	{  
+	System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+	}
+	
+	for(int j = 97; j <= 122; j++)  
+	{  
+	System.out.println(" The ASCII value of " + (char)j + "  =  " + j);  
+	} 
+	
+	}   
+}
