2406a482f5fbb77088697b5a1d93f6479a1fc24e
OviyaGunasekaran <107669821+OviyaGunasekaran@users.noreply.github.com>
2022-08-25 19:20:51 +0530

    X shaped Number Print in Reverse Order

---
+++ NumberPrintXreverse.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class NumberPrintXreverse {
+
+	public static void main(String args[])  
+	{  
+		System.out.print("Enter number: ");
+		Scanner sc = new Scanner(System.in);  
+		String num = sc.next();
+			
+		for (int i = 0; i < num.length() ; i++)
+	    {
+	        for (int j = 0; j < num.length(); j++)
+	        {
+	            if(i==j)
+	            	System.out.print(num.charAt(i));
+	            
+	            else if((i+j) == (num.length()-1))
+	            	System.out.print(num.charAt(j));
+	            
+	            else
+	            	System.out.print(" ");
+	        }
+	        
+	        System.out.println("\n");   
+	    }
+	}
+}	
+
