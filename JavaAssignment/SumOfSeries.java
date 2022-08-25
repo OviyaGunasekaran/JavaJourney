082e901625249cddd139cf5a35c340c6a33a61dc
OviyaGunasekaran <107669821+OviyaGunasekaran@users.noreply.github.com>
2022-08-25 20:52:07 +0530

    Sum of the Series 1+11+111.. pgm added

---
+++ JavaAssignment/SumOfSeries.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SumOfSeries {
+	public static void main(String[] args)
+    {
+		System.out.print("Enter the number of terms: ");  
+		Scanner sc = new Scanner(System.in);  
+	    int num = sc.nextInt();
+	    
+	    int sum = 0, j = 1;
+	    
+        for (int i = 1; i <= num; i++)
+        {
+            sum = sum + j;
+            j = (j * 10) + 1;
+        }
+        System.out.print("Sum of the series: "+sum);  
+	}
+
+}
