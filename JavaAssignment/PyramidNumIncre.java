59b16f449dde1bf251b138a7f86c83c3f2cffef6
OviyaGunasekaran <107669821+OviyaGunasekaran@users.noreply.github.com>
2022-08-28 18:48:49 +0530

    Pyramid Pattern for Entered Number pgm added

---
+++ JavaAssignment/PyramidNumIncre.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PyramidNumIncre {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);  
+		System.out.println("Enter a number: "); 
+		int num = sc.nextInt(); 
+		
+		int i=1,j,k=1;
+		
+		while(k<=num)
+		{
+			for (j=i;j<(num-1)/2;j++)
+			{
+				System.out.print(" ");
+			}
+			for (j=1;j<=i;j++)
+			{
+				System.out.print(k++ + " ");
+			}
+		    System.out.println("\n");
+		    i++;
+		}
+	}
+}
