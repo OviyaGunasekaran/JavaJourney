8e1e5d26ab8980105aae2dbe471daba1ec88a5d2
OviyaGunasekaran <107669821+OviyaGunasekaran@users.noreply.github.com>
2022-09-01 22:13:40 +0530

    Pattern Print Equilateral Triangle pgm added

---
+++ JavaAssignment/Set2_4EquilatTri.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Set2_4EquilatTri {
+	
+	public static void main(String[] args)
+    {
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        int i,j;
+        
+        for (i=0; i<n; i++)
+        { 
+        	for (j=n-i; j>1; j--)
+        	{ 
+               System.out.print(" ");
+            } 
+            for (j=0; j<=i; j++ )
+            { 
+                System.out.print("* ");
+            } 
+  
+            System.out.println();
+        } 
+    } 
+}
